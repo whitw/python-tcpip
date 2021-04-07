f66ac007d3748da1ad8bfa79879b3879830ebe58
whitw <shkim7876@naver.com>
2021-04-07 18:56:31 +0000

    simple terminal based single server multi client chatting program

--- demo-multi/client.py
+++ demo-multi/client.py
@@ -1,5 +1,7 @@
 import socket
 import random
+import time
+import struct
 import threading
 import re
 
@@ -22,22 +24,65 @@ def send(client_socket, name, color, data):
     client_socket.send(msg)
     return
 
-def recv(client_socket, data):
+def recv(client_socket):
     #packet format:
     #num_packet(1)
     #repeat num_packet times:
     #  color(3)
     #  len_name(1)
-    #  name(len_name)
-    #  time(4)
+    #  name(len_name(max=256))
+    #  time(8)
     #  len_conteant(4)
-    #  content(len_content)
-    return None
-    data = 'testmsg'.encode('utf-8')
-    return data.decode('utf-8')
+    #  content(len_content(max=65536))
+    '''
+    this code is not implemented yet. code down below is just a test code
+    '''
+    result = []
+    try:
+        num_packet = int.from_bytes(client_socket.recv(1),'big')
+    except socket.timeout as e:
+        raise e
+    except ConnectionResetError:
+        return
+    if not num_packet:
+        return
+    for d in range(num_packet):
+        color = '0x' + client_socket.recv(3).hex()
+        len_name = int.from_bytes(client_socket.recv(1),'big')
+        name = client_socket.recv(len_name).decode('utf-8')
+        time = struct.unpack('d',client_socket.recv(8))[0]
+        len_content = int.from_bytes(client_socket.recv(4),'big')
+        contents = client_socket.recv(len_content).decode('utf-8')
+        result.append([color, name, time, contents])
+    return result
 
-def recv_chat():
-    pass
+def recv_chat(client_socket):
+    while(True):
+        try:
+           data = recv(client_socket)
+        except socket.timeout:
+            pass
+        if not data:
+            time.sleep(0.1)
+            continue
+        '''
+        this code is not implemented yet. code down below is just a test code
+        '''
+        for d in data:
+            #d[0]:color(in '0x000000' formatted str, ignored on termnal circmustance)
+            #d[1]:name(in str)
+            #d[2]:time(in gmt time.time() format)
+            #d[3]:contents(in str)
+            d[2] = time.localtime(d[2])
+            tm_hr,tm_min,tm_sec=d[2].tm_hour, d[2].tm_min, d[2].tm_sec
+            print('\n{:02}:{:02}:{:02}]{}:{}'.format(tm_hr,tm_min,tm_sec,d[1],d[3]))
+
+def send_chat(client_socket, name, color):
+    while(True):
+        msg = input('')
+        if not msg:
+            break
+        send(client_socket, name, color, msg)
 
 def main():
     color_re = re.compile('[0-9a-fA-F]{6}')
@@ -67,11 +112,11 @@ def main():
         if name:
             break
 
-    while(True):
-        msg = input('Send:')
-        if not msg:
-            break
-        send(client_socket, name, color, msg)
+    inpt = threading.Thread(target=send_chat, args=(client_socket,name,color), daemon=True)
+    outpt = threading.Thread(target=recv_chat, args=(client_socket,), daemon=True)
+    inpt.start()
+    outpt.start()
+    inpt.join()
     client_socket.close()
 
 if __name__ == '__main__':
--- demo-multi/server.py
+++ demo-multi/server.py
@@ -1,4 +1,5 @@
 import time
+import struct
 import socket
 import threading
 
@@ -23,19 +24,48 @@ begin_idx_each = {}
 def dbg(name, param):
     print('{}={}{}'.format(name,param,type(param)))
 
-def send(client_socket, addr):
-    
+def send(client_socket, addr, my_begin_idx):
     #packet format:
     #num_packet(1)
     #repeat num_packet times:
     #  color(3)
     #  len_name(1)
     #  name(len_name)
-    #  time(4)
+    #  time(8)
     #  len_content(4)
     #  content(len_content)
+    num_success = 0
+    if(my_begin_idx < begin_idx):
+        my_begin_idx = begin_idx
+    num_packet = len(log) + begin_idx - my_begin_idx
+    if num_packet == 0:
+        #nothing to send, quit.
+        return 0
 
-    return None
+    try:
+        client_socket.send(int.to_bytes(num_packet, 1, 'big'))
+    except Exception as e:
+        print('Error on sending the number of packet.')
+        raise e
+
+    from_idx = my_begin_idx - begin_idx
+    end_idx = len(log)
+    for d in range(from_idx, end_idx):
+        data = log[d]
+        msg = data[0] #color, bytes, len=3
+        name = data[1].encode('utf-8')
+        msg = msg + int.to_bytes(len(name),1,'big') + name
+        msg = msg + struct.pack('d',data[2]) # time, double to bytes, len=8
+        content = data[3].encode('utf-8')
+        msg = msg + int.to_bytes(len(content),4,'big') + content
+        try:
+            client_socket.send(msg)
+        except OSError as e:
+            print('failed to send log:',log[d])
+            raise e
+        else:
+            num_success += 1
+    return num_success
 
 def recv(client_socket, addr):
     #packet format:
@@ -48,6 +78,8 @@ def recv(client_socket, addr):
         data = client_socket.recv(4)
     except socket.timeout as e:
         raise e
+    except ConnectionResetError:
+        return
     if not data:
         return
     color = data[:3]
@@ -58,11 +90,11 @@ def recv(client_socket, addr):
     content = client_socket.recv(len_content).decode('utf-8')
     tm = time.time()
     msg = (color, name, tm, content)
-    print(msg)
+    dbg('msg',msg)
     return msg
 
 def manage_sock(client_socket, addr, lock):
-    begin_idx_each[addr] = 0
+    begin_idx_each[addr] = begin_idx
     print("connected:", addr)
     while(True):
         try:
@@ -77,7 +109,8 @@ def manage_sock(client_socket, addr, lock):
                 log.append(data)
                 lock.release()
 
-        send(client_socket, addr)
+        num_success = send(client_socket, addr,begin_idx_each[addr])
+        begin_idx_each[addr] += num_success
     print('disconnected:',addr)
     client_socket.close()
 
