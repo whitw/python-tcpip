88e7c6c70b33f2bd778a4256dc70585bcdae0649
whitw <shkim7876@naver.com>
2021-04-07 08:41:42 +0000

    demo-multi, client->server test

--- demo-multi/client.py
+++ demo-multi/client.py
@@ -1,15 +1,79 @@
 import socket
+import random
+import threading
+import re
+
 host = 'localhost'
 port = 5000
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((host, port))
-while(True):
-    msg = input('Send:')
-    if not msg:
-        break
+
+
+def send(client_socket, name, color, data):
+    #packet format:
+    #color(3)
+    #len_name(1)
+    #name(len_name)
+    #len_content(4)
+    #content(len_content)
+    msg = int.to_bytes(color,3,'big')
+    name = name.encode('utf-8')
+    data = data.encode('utf-8')
+    msg = msg + int.to_bytes(len(name),1,'big') + name
+    msg = msg + int.to_bytes(len(data),4,'big') + data 
+    client_socket.send(msg)
+    return
+
+def recv(client_socket, data):
+    #packet format:
+    #num_packet(1)
+    #repeat num_packet times:
+    #  color(3)
+    #  len_name(1)
+    #  name(len_name)
+    #  time(4)
+    #  len_conteant(4)
+    #  content(len_content)
+    return None
+    data = 'testmsg'.encode('utf-8')
+    return data.decode('utf-8')
+
+def recv_chat():
+    pass
+
+def main():
+    color_re = re.compile('[0-9a-fA-F]{6}')
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        client_socket.send(msg.encode())
-    except Exception as e:
-        print("exception occured:",e)
-client_socket.close()
+        client_socket.connect((host, port))
+    except ConnectionRefusedError:
+        print("Cannot connect to server... Quit.")
+        return
+
+    while(True):
+        color = input('Set your color: #')
+        if color.lower() == 'random':
+            color = random.randint(0, 16**6)
+            break
+        if not color_re.match(color):
+            print('invalid color')
+            print('please input your color in: #000000 format')
+            print('or type "random" to select random color')
+            continue
+        else:
+            color = int(color.lower(),base=16)
+            break
+
+    while(True):
+        name = input('Set your name:')
+        if name:
+            break
+
+    while(True):
+        msg = input('Send:')
+        if not msg:
+            break
+        send(client_socket, name, color, msg)
+    client_socket.close()
+
+if __name__ == '__main__':
+    main()
 
--- demo-multi/server.py
+++ demo-multi/server.py
@@ -1,9 +1,18 @@
+import time
 import socket
 import threading
 
 server = 'localhost'
 port = 5000
 
+max_storage_size = 300
+#very first index of the log(to set maximum storage size)
+begin_idx = 0
+#chatting log, log[i] = (color, name, time, content)
+log = []
+
+#begin_idx_each: {client_addr:begin_idx}
+begin_idx_each = {}
 
 #if you are running this on separate server...
 #with open('host.txt', 'rt') as f:
@@ -11,28 +20,85 @@ port = 5000
 #    port = int(f.readline().strip())
 #    print(server,port)
 
-def manage_sock(client_socket, addr):
-    print("addr:", addr)
+def dbg(name, param):
+    print('{}={}{}'.format(name,param,type(param)))
+
+def send(client_socket, addr):
+    
+    #packet format:
+    #num_packet(1)
+    #repeat num_packet times:
+    #  color(3)
+    #  len_name(1)
+    #  name(len_name)
+    #  time(4)
+    #  len_content(4)
+    #  content(len_content)
+
+    return None
+
+def recv(client_socket, addr):
+    #packet format:
+    #color(3)
+    #len_name(1)
+    #name(len_name)
+    #len_content(4)
+    #content(len_content)
+    try:
+        data = client_socket.recv(4)
+    except socket.timeout as e:
+        raise e
+    if not data:
+        return
+    color = data[:3]
+    len_name = data[3]
+    name = client_socket.recv(len_name).decode('utf-8')
+    len_content = client_socket.recv(4)
+    len_content = int.from_bytes(len_content, 'big')
+    content = client_socket.recv(len_content).decode('utf-8')
+    tm = time.time()
+    msg = (color, name, tm, content)
+    print(msg)
+    return msg
+
+def manage_sock(client_socket, addr, lock):
+    begin_idx_each[addr] = 0
+    print("connected:", addr)
     while(True):
-        data = client_socket.recv(65536)
-        if not data:
-            break
-        print("%s:%s" % (addr, data.decode()))
-    print('%s: disconnected' % addr)
+        try:
+            data = recv(client_socket, addr)
+        except socket.timeout:
+            pass
+        else:
+            if not data:
+                break
+            else:
+                lock.acquire()
+                log.append(data)
+                lock.release()
+
+        send(client_socket, addr)
+    print('disconnected:',addr)
     client_socket.close()
 
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((server,port))
+    print('waiting for client...')
+    client_list = []
+    server_socket.listen(0)
+    _lock = threading.Lock()
+    while(True):
+        client_socket, addr = server_socket.accept()
+        client_socket.settimeout(1)
+        print("connected, forking...")
+        th = threading.Thread(target=manage_sock,
+                         args=(client_socket,addr,_lock),
+                         daemon=True)
+        th.start()
+        client_list.append(th)
+    server_socket.close()
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((server,port))
-print('waiting for client...')
-client_list = []
-server_socket.listen(0)
-while(True):
-    client_socket, addr = server_socket.accept()
-    print("connected, forking...")
-    th = threading.Thread(target=manage_sock,
-                     args=(client_socket,addr),
-                     daemon=True)
-    th.start()
-    client_list.append(th)
-server_socket.close()
+if __name__ == '__main__':
+    main()
