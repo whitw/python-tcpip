3b91d6faa592e8d23a7c46e178f8a0e69329b256
whitw <shkim7876@naver.com>
2021-04-08 02:54:32 +0000

    added server open message

--- demo-multi/server.py
+++ demo-multi/server.py
@@ -119,6 +119,7 @@ def main():
         pass
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("Server opened at", server,port)
     server_socket.bind((server,port))
     print('waiting for client...')
     client_list = []
